d328b7943652c4ce6f6c27e87719ba1058cf2917
Michelle Baert <rocky.road@rocky-shore.net>
2016-09-22 21:47:01 +0200

    * Using application settings from package.json     * Using Pug template engine for views     * Serving static files

--- app.js
+++ app.js
@@ -1,11 +1,47 @@
 var express = require('express');
+var path = require('path');
+
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+// ============ Settings
+// == get port from environment if exists
+// Note: use 0 for random port
+app.set('port', process.env.PORT || 5000);
+
+// == HTML templates
+app.set('view engine', 'pug');
+
+// == Displayed application name
+app.set('name', process.env.npm_package_name);
+app.set('version', process.env.npm_package_version)
+
+// ============= Early middleware
+// == Logging requests
+app.use(function (req, res, next) {
+  console.log('Requested: %s %s', req.method, req.url);
+  next();
 });
 
-// using random port
-var srv = app.listen(0, function () {
-    console.log('Example app available on http://localhost:%s', srv.address().port);
+// ============= Routes
+// == Home page
+app.get('/', function (req, res) {
+  res.render('index', {
+    title: app.get('name') + " v" + app.get('version'),
+    message: process.env.npm_package_description
+  });
+});
+
+// ============= Late middleware
+// == serving static files
+app.use(express.static(path.join(__dirname, 'static')));
+
+// ============= HTTP server
+var srv = app.listen(app.get('port'), function () {
+  var addr = srv.address();
+  console.log('%s app version %s available on %s http://%s:%s',
+              app.get('name'),
+              app.get('version'),
+              addr.family,
+              addr.address,
+              addr.port);
 });
